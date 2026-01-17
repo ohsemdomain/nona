01028d0fa34a6301170eebadf476687973e74116
ohsemdomain <ohsemdomain@gmail.com>
2026-01-17 18:34:37 +0800

    refactor(query): change detail key functions to accept number id

--- src/lib/queryKey.ts
+++ src/lib/queryKey.ts
@@ -10,18 +10,18 @@ export const queryKey = {
 		all: ["category"] as const,
 		list: (param?: Record<string, unknown>) =>
 			["category", "list", param] as const,
-		detail: (id: string) => ["category", "detail", id] as const,
+		detail: (id: number) => ["category", "detail", id] as const,
 	},
 	item: {
 		all: ["item"] as const,
 		list: (param?: Record<string, unknown>) => ["item", "list", param] as const,
-		detail: (id: string) => ["item", "detail", id] as const,
+		detail: (id: number) => ["item", "detail", id] as const,
 	},
 	order: {
 		all: ["order"] as const,
 		list: (param?: Record<string, unknown>) =>
 			["order", "list", param] as const,
-		detail: (id: string) => ["order", "detail", id] as const,
+		detail: (id: number) => ["order", "detail", id] as const,
 	},
 	role: {
 		all: ["role"] as const,
